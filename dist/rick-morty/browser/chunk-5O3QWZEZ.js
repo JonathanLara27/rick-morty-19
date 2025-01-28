02c2e206e34cd6ca2425ae74bfc0e6f64b2aab03
Jonathan Lara <jonathan.lara@unmsm.edu.pe>
2025-01-28 01:40:49 -0500

    [TEST] Subiendo dist-

---
+++ dist/rick-morty/browser/chunk-5O3QWZEZ.js
@@ -0,0 +1,2 @@
+import{Pb as o,ab as i,wb as p,wc as m,xb as s}from"./chunk-ERFRTTSI.js";var t=class n{static \u0275fac=function(e){return new(e||n)};static \u0275cmp=i({type:n,selectors:[["app-episode"]],decls:3,vars:0,template:function(e,c){e&1&&(p(0,"p"),o(1,"episode works!"),s(),o(2,`,
+`))},dependencies:[m],styles:["[_nghost-%COMP%]{display:block}"],changeDetection:0})};export{t as default};
---
+++ dist/rick-morty/browser/chunk-MRPVYU7H.js
@@ -0,0 +1,2 @@
+import{Pb as t,ab as a,wb as c,wc as m,xb as r}from"./chunk-ERFRTTSI.js";var n=class o{static \u0275fac=function(e){return new(e||o)};static \u0275cmp=a({type:o,selectors:[["app-character"]],decls:3,vars:0,template:function(e,p){e&1&&(c(0,"p"),t(1,"character works!"),r(),t(2,`,
+`))},dependencies:[m],styles:["[_nghost-%COMP%]{display:block}"],changeDetection:0})};export{n as default};
